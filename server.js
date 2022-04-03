09dc4418d1f29632ec5fda2e01ee752be45fc34f
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2022-04-03 14:09:02 -0400

    Get, Post, Delete routes functional

--- server.js
+++ server.js
@@ -22,6 +22,64 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(db);
+    })
+
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        let highestId = 99;
+        for (let i = 0; i < db.length; i++) {
+            let individualNote = db[i];
+
+            if (individualNote.id > highestId) {
+              
+                highestId = individualNote.id;
+            }
+        }
+       
+        newNote.id = highestId + 1;
+      
+        db.push(newNote)
+
+
+        fs.writeFile(jsonFilePath, JSON.stringify(db), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+        });
+        
+        res.json(newNote);
+    });
+
+
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    
+    for (let i = 0; i < db.length; i++) {
+
+        if (db[i].id == req.params.id) {
+           
+            db.splice(i, 1);
+            break;
+        }
+    }
+    
+    fs.writeFileSync(jsonFilePath, JSON.stringify(db), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Note deleted");
+        }
+    });
+    res.json(db);
+});
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
